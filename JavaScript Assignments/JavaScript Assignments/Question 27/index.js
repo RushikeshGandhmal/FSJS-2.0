aa414ddb0529b8f8000a6deba1828c0d1b40612a
Rushi <rushikeshgandhmal@gmail.com>
2023-02-18 20:18:12 +0530

    Solved All JavaScript Questions

--- JavaScript Assignments/JavaScript Assignments/Question 27/index.js
+++ JavaScript Assignments/JavaScript Assignments/Question 27/index.js
@@ -1,26 +1,23 @@
 // 27. Write a program to print all the prime number between 0 to 100 (0 and 100 included).
 
 function printPrimeNumber(num) {
-    if (num <= 1) {
-        console.log('Please enter a number greater than 1');
-        return
-    } else {
-
-        for (let i = 2; i <= num; i++) {
-            let isPrime = true;
-            for (let j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j === 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) {
-                console.log(i);
-            }
-
+  if (num <= 1) {
+    console.log("Please enter a number greater than 1");
+    return;
+  } else {
+    for (let n = 2; n <= num; n++) {
+      let isPrime = true;
+      for (let i = 2; i * i <= n; i++) {
+        if (n % i === 0) {
+          isPrime = false;
+          break;
         }
+      }
+      if (isPrime) {
+        console.log(n);
+      }
     }
+  }
 }
-// user input
-let num = 100;
-printPrimeNumber(num);
+
+printPrimeNumber(100);
